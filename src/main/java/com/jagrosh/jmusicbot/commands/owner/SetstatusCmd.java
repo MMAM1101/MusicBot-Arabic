1e358d2e12dcffdb8dfde7e3c72fe8f2eb98019e
MMAM1101 <abdyksa54@gmail.com>
2026-06-13 19:27:14 +0300

    Add src/main/java/com/jagrosh/jmusicbot/commands/owner/SetstatusCmd.java

---
+++ src/main/java/com/jagrosh/jmusicbot/commands/owner/SetstatusCmd.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017 John Grosh <john.a.grosh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jagrosh.jmusicbot.commands.owner;
+
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.jagrosh.jmusicbot.Bot;
+import com.jagrosh.jmusicbot.commands.OwnerCommand;
+import net.dv8tion.jda.api.OnlineStatus;
+
+/**
+ *
+ * @author John Grosh <john.a.grosh@gmail.com>
+ */
+public class SetstatusCmd extends OwnerCommand
+{
+    public SetstatusCmd(Bot bot)
+    {
+        this.name = "تعيين_الحالة";
+        this.help = "يحدد حالة ظهور البوت";
+        this.arguments = "<status>";
+        this.aliases = bot.getConfig().getAliases(this.name);
+        this.guildOnly = false;
+    }
+    
+    @Override
+    protected void execute(CommandEvent event) 
+    {
+        try {
+            OnlineStatus status = OnlineStatus.fromKey(event.getArgs());
+            if(status==OnlineStatus.UNKNOWN)
+            {
+                event.replyError("يرجى إدخال إحدى الحالات التالية: `ONLINE`, `IDLE`, `DND`, `INVISIBLE`");
+            }
+            else
+            {
+                event.getJDA().getPresence().setStatus(status);
+                event.replySuccess("تم تعيين الحالة إلى `"+status.getKey().toUpperCase()+"`");
+            }
+        } catch(Exception e) {
+            event.reply(event.getClient().getError()+" The status could not be set!");
+        }
+    }
+}
