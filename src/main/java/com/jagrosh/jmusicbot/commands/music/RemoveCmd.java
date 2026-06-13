7694cfab9d18397a38b60e18f76fe4e6b2b02f3b
MMAM1101 <abdyksa54@gmail.com>
2026-06-13 19:27:07 +0300

    Add src/main/java/com/jagrosh/jmusicbot/commands/music/RemoveCmd.java

---
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/RemoveCmd.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2016 John Grosh <john.a.grosh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jagrosh.jmusicbot.commands.music;
+
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.jagrosh.jmusicbot.Bot;
+import com.jagrosh.jmusicbot.audio.AudioHandler;
+import com.jagrosh.jmusicbot.audio.QueuedTrack;
+import com.jagrosh.jmusicbot.commands.MusicCommand;
+import com.jagrosh.jmusicbot.settings.Settings;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.User;
+
+/**
+ *
+ * @author John Grosh <john.a.grosh@gmail.com>
+ */
+public class RemoveCmd extends MusicCommand 
+{
+    public RemoveCmd(Bot bot)
+    {
+        super(bot);
+        this.name = "حذف";
+        this.help = "يحذف أغنية من القائمة";
+        this.arguments = "<position|ALL>";
+        this.aliases = bot.getConfig().getAliases(this.name);
+        this.beListening = true;
+        this.bePlaying = true;
+    }
+
+    @Override
+    public void doCommand(CommandEvent event) 
+    {
+        AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler();
+        if(handler.getQueue().isEmpty())
+        {
+            event.replyError("لا يوجد شيء في القائمة!");
+            return;
+        }
+        if(event.getArgs().equalsIgnoreCase("all"))
+        {
+            int count = handler.getQueue().removeAll(event.getAuthor().getIdLong());
+            if(count==0)
+                event.replyWarning("لا يوجد لديك أي أغاني في القائمة!");
+            else
+                event.replySuccess("تم حذف "+count+" مقطع.");
+            return;
+        }
+        int pos;
+        try {
+            pos = Integer.parseInt(event.getArgs());
+        } catch(NumberFormatException e) {
+            pos = 0;
+        }
+        if(pos<1 || pos>handler.getQueue().size())
+        {
+            event.replyError("يجب أن يكون الموضع رقمًا صحيحًا بين 1 و"+handler.getQueue().size()+"!");
+            return;
+        }
+        Settings settings = event.getClient().getSettingsFor(event.getGuild());
+        boolean isDJ = event.getMember().hasPermission(Permission.MANAGE_SERVER);
+        if(!isDJ)
+            isDJ = event.getMember().getRoles().contains(settings.getRole(event.getGuild()));
+        QueuedTrack qt = handler.getQueue().get(pos-1);
+        if(qt.getIdentifier()==event.getAuthor().getIdLong())
+        {
+            handler.getQueue().remove(pos-1);
+            event.replySuccess("تمت إزالة **"+qt.getTrack().getInfo().title+"** from the queue");
+        }
+        else if(isDJ)
+        {
+            handler.getQueue().remove(pos-1);
+            User u;
+            try {
+                u = event.getJDA().getUserById(qt.getIdentifier());
+            } catch(Exception e) {
+                u = null;
+            }
+            event.replySuccess("تمت إزالة **"+qt.getTrack().getInfo().title
+                    +"** from the queue (requested by "+(u==null ? "أحد ما" : "**"+u.getName()+"**")+")");
+        }
+        else
+        {
+            event.replyError("لا يمكنك إزالة **"+qt.getTrack().getInfo().title+"** لأنك لم تقم بإضافتها!");
+        }
+    }
+}
